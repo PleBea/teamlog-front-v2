70b6e76f407a273e0f7b829ab48cc163d5a984f7
PleBea <bini060315kim@gmail.com>
2022-09-03 04:37:35 +0900

    bug fix

--- components/SpecialItem.vue
+++ components/SpecialItem.vue
@@ -2,7 +2,7 @@
     <section class="section special" id="special">
         <div class="special_list">
             <div class="special_item">
-                <div 
+                <div
                 class="special_items"
                 v-for="(i, k) in special"
                 :key="k">
@@ -13,7 +13,7 @@
             </div>
             <div class="special_description">
                 <h1>저희는 <strong>이러한 특색</strong>을<br />가진 동아리입니다.</h1>
-                <p>저희는 좋은 교육방향, 분위기, 성장을 전부 담아<br />
+                <p>저희는 좋은 {{special[0].title}}, {{special[1].title}}, {{special[2].title}}을 전부 담아<br />
                 매력적인 특색을 가진 동아리로 올라가고 있습니다. 또한<br />
                 모두가 만족스러운 특색을 가질 수 있도록 노력하고 있습니다.
                 </p>
@@ -28,15 +28,12 @@ import { special } from '@/types/index'
 const special_json = require('@/static/resources/information.json').special
 
 export default Vue.extend({
-    name: 'SpecialItem',
-    data() {
-        return {
-            special: [] as special[]
-        }
-    },
-    mounted() {
-        this.special = special_json as special[]
+  name: 'SpecialItem',
+  data() {
+    return {
+      special: special_json as special[]
     }
+  },
 })
 </script>
 
